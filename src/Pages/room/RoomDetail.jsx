8454dedb0f73e62f64b72be8a36424f6dd8353f9
jobkaeHenry <1zzangjun@gmail.com>
2022-11-13 03:34:26 +0900

    detail updated

--- src/Pages/room/RoomDetail.jsx
+++ src/Pages/room/RoomDetail.jsx
@@ -1,63 +1,88 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../../Components/Header";
 import UserReview from "../../Components/Auth/room/UserReview";
 import axios from "axios";
+import { ImageElem, ImageWrapper, RowWrapper } from "../../Components/Main/HouseCard";
+import { ReviewStar } from "../../Hooks/ReviewStar";
+import styled from "@emotion/styled";
+
+const Centering = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  margin: 0 auto;
+`;
+const DistanceVisual = styled.div`
+    width: 684px;
+
+`
 
 const RoomDetail = () => {
+  const [starData, setStarData] = useState(null);
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  // axios로 리뷰받기
+  const getReview = async () => {
+    try {
+      axios
+        .get("/room/detail/1/review", {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+        .then((res) => {
+          console.log(res.data);
+          setStarData(res.data);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const getDetail = async () => {
+    try {
+      axios
+        .get("/room/1", {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+        .then((res) => {
+          console.log(res.data);
+          setData(res.data);
+          setIsLoading(false);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-    const [starData, setStarData] = useState(null);
-    const [data,setData]=useState(null)
-    const [isLoading, setIsLoading] = useState(true);
-    // axios로 리뷰받기
-    const getReview = async () => {
-      try {
-        axios
-          .get("/room/detail/1/review", {
-            headers: {
-              "Content-Type": "application/json",
-            },
-          })
-          .then((res) => {
-            console.log(res.data);
-            setStarData(res.data);
-
-          });
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    const getDetail = async () => {
-        try {
-          axios
-            .get("/room/detail/1/review", {
-              headers: {
-                "Content-Type": "application/json",
-              },
-            })
-            .then((res) => {
-              console.log(res.data);
-              setData(res.data);
-              setIsLoading(false);
-            });
-        } catch (err) {
-          console.log(err);
-        }
-      };
-
-    useEffect(() => {
-      getReview();
-      getDetail()
-    }, []);
-  return !isLoading?(
+  useEffect(() => {
+    getReview();
+    getDetail();
+  }, []);
+  return !isLoading ? (
     <>
-    <Header ></Header>
-      <div>RoomDetail</div>
-      {starData.map((e)=>{
-        return<UserReview key={e.reviewID} data={e}/>
+      <Header title={data.name} />
+
+      <ImageWrapper
+        className="mb-10"
+        src={data.images}
+        alt={`${data.title} 의 대표사진`}
+      >
+        <ImageElem src={data.images} alt={`${data.name} 의 대표사진`} />
+      </ImageWrapper>
+      <Centering>
+        <ReviewStar starNum={Math.floor(data.average)} />
+        <RowWrapper>
+        <sapn className="h4 bold">{data.average}</sapn><span className="h4 gray"> / 5</span>
+        </RowWrapper>
+      </Centering>
+      {starData.map((e) => {
+        return <UserReview key={e.reviewID} data={e} />;
       })}
-      
     </>
-  ):null;
+  ) : null;
 };
 
 export default RoomDetail;
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -72,7 +72,92 @@ export const handlers = [
     ])
     );
   }),
-  rest.post("/room/detail/1/review", async (req, res, ctx) => {
+  rest.get("/room/detail/2/review", async (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+        reviewId:1,
+        reviewStar: 4,
+        text: "밤에 소음이 좀 들어오는거 같은데 저렴한 방이니 이해하렵니다",
+        date: "2022-11-12",
+      },
+      {
+        reviewId:2,
+        reviewStar: 3,
+        text: "냄새가 조금 납니다",
+        date: "2022-11-11",
+      },
+      {
+        reviewId:2,
+        reviewStar: 3,
+        text: "집주인이 연락이 잘 안됩니다",
+        date: "2022-11-10",
+      },
+    ])
+    );
+  }),
+  rest.get("/room/detail/3/review", async (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+        reviewId:1,
+        reviewStar: 4,
+        text: "밤에 소음이 좀 들어오는거 같은데 저렴한 방이니 이해하렵니다",
+        date: "2022-11-12",
+      },
+      {
+        reviewId:2,
+        reviewStar: 3,
+        text: "냄새가 조금 납니다",
+        date: "2022-11-11",
+      },
+      {
+        reviewId:2,
+        reviewStar: 3,
+        text: "집주인이 연락이 잘 안됩니다",
+        date: "2022-11-10",
+      },
+    ])
+    );
+  }),
+  rest.get("/room/1", async (req, res, ctx) => {
+    return res(
+      ctx.json({
+        home:1,
+        user:"junguZzang",
+        name:"소라빌라",
+        images:"https://picsum.photos/640/360",
+        created_at:"2022-10-13",
+        average:4.18,
+        rankList:[
+          {
+            safe:4,
+            clean:5,
+            heat:1,
+            noise:3,
+            option:4,
+            sunny:2,
+          }
+        ],
+        size:20.3,
+        price:
+          {
+            montly_rent:30,
+            deposit:500,
+            administration_cost:5
+          },
+        hitter:"중앙난방",
+        room:1,
+        floor:1,
+        parkink:3,
+        veranda:"유",
+        number:"01054072694",
+        distance:150,
+        option:"애완견 가능, 냉장고, 세탁기, 전자레인지"
+      })
+    );
+  }),
+  rest.post("/room", async (req, res, ctx) => {
     return res(ctx.status(200));
   }),
 ];

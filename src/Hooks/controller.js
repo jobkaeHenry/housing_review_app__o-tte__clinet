cefd922a8139fe0503bd28809ed96f1adf582f4e
bookhabit <qnrgoqlt@naver.com>
2022-11-12 23:57:14 +0900

    review component handlers

--- src/Hooks/controller.js
+++ src/Hooks/controller.js
@@ -1,9 +1,37 @@
-
 // 의미없는 테스트용 함수입니다
 
+import styled from "@emotion/styled";
+
 function isOdd(elem) {
-  return Boolean(elem%2)
+  return Boolean(elem % 2);
 }
 
+const Yellow = styled.span`
+  color: #eab64d;
+`;
+const Gray = styled.span`
+  color: var(--font-gray);
+`;
 
-export default isOdd
+export const ReviewStar = (starNum) => {
+  console.log(starNum);
+  let filledStar;
+  let noStar;
+
+  for (let i = 0; i < starNum; i++) {
+    console.log(i);
+    filledStar += "★";
+  }
+  console.log(filledStar);
+  for (let j = 0; j < 5 - starNum; j++) {
+    noStar += "☆";
+  }
+  return (
+    <>
+      <Yellow>{filledStar}</Yellow>
+      <Gray>{noStar}</Gray>
+    </>
+  );
+};
+
+export default isOdd;
